ea397c8d356f500975e3559d973cc1c0605535c4
shiva-metageeks <shiva@metageeks.tech>
2024-09-11 05:26:46 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,17 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+        <div className="flex justify-between items-center">
+          <Image
+            className=""
+            src="https://media.licdn.com/dms/image/v2/D5603AQE6t9UR0HWzXQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1704110686462?e=1731542400&v=beta&t=u5EwLfd0zH-G2ngFEpRiV1UPzYGrf94u31z6h_4Hyho"
+            alt="Next.js logo"
+            width={180}
+            height={38}
+            priority
+          />
+          <h1 className="text-xl">Shiva Shah</h1>
+        </div>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
